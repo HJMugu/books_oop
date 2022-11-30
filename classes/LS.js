04e35acf9d06c3e752d56b089461522ea01f2baa
Henri <henri-joel.mugu@voco.ee>
2022-11-30 09:49:39 +0200

    Refactored addBook LS functionality

--- classes/LS.js
+++ classes/LS.js
@@ -1,12 +1,26 @@
 class LS {
-    addBook(book){
-        let books;
-        if(localStorage.getItem('books') === null){
-            books = [];
+
+
+
+    //help functions get and set from ls
+    getData(name){
+        let data;
+        if(localStorage.getItem(name) === null){
+            data = [];
         } else {
-            books = JSON.parse(localStorage.getItem('books'));
+            data = JSON.parse(localStorage.getItem(name));
         }
+        return data
+    }
+
+    setData(name, data){
+        localStorage.setItem(name, JSON.stringify(data));
+    }
+
+
+    addBook(book){
+        let books = this.getData('books')
         books.push(book);
-        localStorage.setItem('books', JSON.stringify(books));
+        this.setData('books', books);
     }
 }
